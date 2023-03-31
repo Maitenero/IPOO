97d9bad972df8f23836e649ffc17ddaac1825874
Maitenero <ticho221@hotmail.com>
2023-03-30 23:53:50 -0300

    Archivos del entregable nro1

---
+++ TestViajes.php
@@ -0,0 +1,367 @@
+<?php
+include 'Viaje.php';//incluyo el archivo de la clase viaje
+
+//Creo el array de pasajeros ------PREGUNTAR-------- es asi? jjsdj
+$pasajeros = array (
+    $pasajeros[1]  = ["Nombre"=>"Mario","Apellido"=>"Valenzuela","DNI"=>"33057431"],
+    $pasajeros[2]  = ["Nombre"=>"Juan","Apellido"=>"Micha","DNI"=>"34849769"],
+    $pasajeros[3]  = ["Nombre"=>"Martin","Apellido"=>"Miralobos","DNI"=>"38106564"], 
+    $pasajeros[4]  = ["Nombre"=>"Roberto","Apellido"=>"Palacios","DNI"=>"44653216"],
+    $pasajeros[5]  = ["Nombre"=>"Martin","Apellido"=>"Coronado","DNI"=>"30660528"],
+    $pasajeros[6]  = ["Nombre"=>"Rocio","Apellido"=>"Palacios","DNI"=>"29700003"],
+    $pasajeros[7]  = ["Nombre"=>"Pedro","Apellido"=>"Berni","DNI"=>"39883267"],
+    $pasajeros[8]  = ["Nombre"=>"Julieta","Apellido"=>"Palacios","DNI"=>"20981558"],
+    $pasajeros[9]  = ["Nombre"=>"Sofia","Apellido"=>"Cuevas","DNI"=>"37750983"],
+    $pasajeros[10]  = ["Nombre"=>"Mario","Apellido"=>"Pergolini","DNI"=>"32201076"],
+    $pasajeros[11]  = ["Nombre"=>"Norbeto","Apellido"=>"De La Sal","DNI"=>"28644442"],
+    $pasajeros[12]  = ["Nombre"=>"Homero J.","Apellido"=>"Simpson","DNI"=>"27451663"], 
+    $pasajeros[13]  = ["Nombre"=>"Ned","Apellido"=>"Flanders","DNI"=>"38698349"],
+    $pasajeros[14]  = ["Nombre"=>"Antonella","Apellido"=>"Rocuzzo","DNI"=>"29034281"],
+    $pasajeros[15]  = ["Nombre"=>"Lionel","Apellido"=>"Messi","DNI"=>"24509044"],
+    $pasajeros[16]  = ["Nombre"=>"Roberto","Apellido"=>"Carlos","DNI"=>"33062348"],
+    $pasajeros[17]  = ["Nombre"=>"Carlos","Apellido"=>"Duende","DNI"=>"31743891"],
+    $pasajeros[18]  = ["Nombre"=>"Sofia","Apellido"=>"Miralobos","DNI"=>"23407994"]
+);
+
+//Funcion para cargar datos numericos
+/*
+@return INT
+*/
+function cargarNumerico (){
+    $numero = trim(fgets(STDIN));
+    $fin = true;
+    do{
+        if (!is_numeric($numero)){
+            echo "Ingrese el dato de forma correcta (usando numeros): \n";
+            $numero = trim(fgets(STDIN));
+        }
+        else{
+            $fin = false;
+        }
+    }while($fin);
+    return $numero;
+}
+
+//Funcion para cargar datos solo con letras
+/*
+@return STRING
+*/
+function cargarLetras (){
+    $letras = trim(fgets(STDIN));
+    $fin = true;
+    do{
+        if (!ctype_alpha($letras)){
+            echo "Ingrese el dato de forma correcta (usando letras): \n";
+            $letras = trim(fgets(STDIN));
+        }
+        else{
+            $fin = false;
+        }
+    }while($fin);
+    return $letras;
+}
+
+//Creo una funcion que permita cargar pasajeros y retorna un array de pasajeros
+/*
+@return ARRAY
+*/
+function cargarLosPasajeros ($tope){
+    $tope = $tope - 1;
+    $arreglo = array ();
+    echo "Desea cargar un pasajero? (si/no): ";
+    $select = trim(fgets(STDIN));
+    echo "\n";
+    while ((strtolower($select)) == "si" && (count($arreglo)) <= $tope){
+        echo "Ingrese el nombre del pasajero: ";
+        $nom = cargarLetras();
+        echo "\n";
+        echo "Ingrese el apellido del pasajero: ";
+        $ap = cargarLetras();
+        echo "\n";
+        echo "Ingrese el numero de documento: ";
+        $nroDni = cargarNumerico();
+        echo "\n";
+        array_push($arreglo, ["Nombre"=>$nom,"Apellido"=>$ap,"DNI"=>$nroDni]);
+        echo "Desea cargar otro pasajero? (si/no) ";
+        $select = trim(fgets(STDIN));
+        echo "\n";
+    }
+    return $arreglo;
+}
+
+//Creo la funcion para crear un nuevo objeto viaje
+/*
+@return OBJET  || ?????? COMO SE ESCRIBIRIA? 
+*/
+function crearViaje (){
+    $codigo = 0;
+    $destino = "";
+    $pasajerosMaximos = 0;
+    $pasjerosViaje = array ();
+    echo "Ingrese el codigo del viaje: \n";
+    $codigo = cargarNumerico ();
+    echo "\n";
+    echo "ingrese el destino del viaje: \n";
+    $destino = cargarLetras();
+    echo "\n";
+    echo "Ingrese la capacidad maxima de pasajeros: \n";
+    $pasajerosMaximos = cargarNumerico (); 
+    echo "\n";
+    $pasjerosViaje = cargarLosPasajeros ($pasajerosMaximos);
+
+    $objeto = new Viaje ($codigo, $destino, $pasajerosMaximos, $pasjerosViaje);
+
+    return $objeto;
+    
+}          
+
+//Creo una funcion que me diga si el objeto existe o no 
+/*
+@return INT
+*/
+function existe ($arregloViajes, $codViaje){
+    $i = 0;
+    $band = true;
+    $indice = -1;
+    while (( $i < (count($arregloViajes))) && $band ){
+        $clave = $arregloViajes[$i]->getCodigoViaje ();
+        if (strcmp($clave, $codViaje) === 0){
+            echo "\n";
+            echo "El viaje fue cargado correctamente. \n";
+            echo "\n";
+            $band = false;
+            $indice = $i;
+        }
+        elseif (((count($arregloViajes)) == $i) && ($indice == -1)) {
+            echo "\n";
+            echo "No se encontro el viaje con ese codigo. \n";
+            echo "\n";
+        }
+        $i++;
+    }
+    return $indice;
+}
+
+//Creo Una funcion que depliega el menu para seleccionar la opcion
+/*
+@return INT
+*/
+function seleccionarOpcionPrincipal (){
+    $corta = false;
+    do{
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "                    Bienvenido A Viaje Feliz                      \n";
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "                           Opciones:                              \n";
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "\n";
+    echo "1. Crear un nuevo viaje. \n";
+    echo "2. Ver o cambiar los datos de un viaje existente. \n";
+    echo "3. Salir. \n";
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "Seleccione la opcion deseada: ";
+    echo "\n";
+    $opcionSelect = trim(fgets(STDIN));
+    if ((is_numeric($opcionSelect)) && $opcionSelect < 4 && $opcionSelect > 0){
+        $corta = true;
+    }
+    else{
+        echo "\n";
+        echo "Ingrese una opcion valida. \n";
+        echo "\n";
+    }
+    }while (!$corta);
+    return $opcionSelect;
+}
+
+//Creo Una funcion que depliega el menu para seleccionar la opcion
+/*
+@return INT
+*/
+function seleccionarOpcionViaje (){
+    $corta = false;
+    do{
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "                       Opciones Del Viaje:                        \n";
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "\n";
+    echo "1. Ver la Informacion del viaje. \n";
+    echo "2. Cambiar el codigo del viaje. \n";
+    echo "3. Cambiar el destino del viaje. \n";
+    echo "4. Cambiar la cantidad maxima de pasajeros. \n";
+    echo "5. Cambiar el Nombre de un pasajero. \n";
+    echo "6. Cambiar el apellido de un Pasajero. \n";
+    echo "7. Cambiar el DNI de un pasajero. \n";
+    echo "8. Modificar todos los datos de un pasajero. \n";
+    echo "9. Agregar un pasajero. \n";
+    echo "10. Volver. \n";
+    echo "11. Salir. \n";
+    echo "///////////////////////////////////////////////////////////////// \n";
+    echo "Seleccione la opcion deseada: ";
+    $opcionSelect = trim(fgets(STDIN));
+    echo "\n";
+    if ((is_numeric($opcionSelect)) && $opcionSelect < 12 && $opcionSelect > 0){
+        $corta = true;
+    }
+    else{
+        echo "\n";
+        echo "Ingrese una opcion valida. \n";
+        echo "\n";
+    }
+    }while (!$corta);
+    return $opcionSelect;
+}
+
+//creo mi array $viajes para almacenar todos los viajes..
+$viajes = array ();
+
+//creo el objeto $viaje1
+$viaje1 = new Viaje (44302, "Mendoza", 35, $pasajeros);
+array_push ($viajes, $viaje1); //lo pusheo a mi array viajes
+
+
+//Programa principal se ejecutan las opciones que el usuario seleccione
+do {
+    //incializo variables a ultilzar en el menu
+    // Declaracion de variables INT $dni, $newDni, $nuevoCodigo, $opcion, $opcionPrincipal | String $nombre, $apellido, $nuevoDestino, $funciono  | Boolean $corte, $valido
+    $dni = 0;
+    $newDni = 0;
+    $nuevoCodigo = 0;
+    $opcionPrincipal = 0;
+    $nombre = " ";
+    $apellido = " ";
+    $nuevoCodigo = "";
+    $funciono = "";
+    $corte = false;
+    $valido = false;
+    $seguir = -1;
+
+    //llamo a funcion del primer menu
+    $opcion = seleccionarOpcionPrincipal();
+
+    switch ($opcion){//switch del primer menu
+        case 1: 
+            $nuevoViaje = crearViaje();
+            echo "El viaje se creo con exito. \n";
+            array_push ($viajes, $nuevoViaje);
+            echo "\n";
+            break;
+        case 2:
+            echo "Escriba el codigo del viaje deseado: \n";
+            $viajeDes = cargarNumerico ();
+            $seguir = existe ($viajes, $viajeDes);
+            echo "\n";
+            if ($seguir == -1){
+                echo "El viaje ".$viajeDes." no pudo ser encontrado, vuelva a intentarlo. \n";
+                echo "\n";
+            }
+            break;
+        case 3:
+            $opcion = 11;
+            break;
+    }
+
+    if ($seguir !== -1){//ingreso solo si la funcion | existe() | me retorna el indice donde se encuentra el obejto en el array
+        do {
+
+            //llamo a funcion del segundo menu
+            $opcion = seleccionarOpcionViaje ();   
+
+            //creo un switch que dependiendo la opcion elegida, ejecuta los metodos necesarios
+            switch ($opcion) {
+                    case 1: //Ver la informacion del viaje
+                    $funciono = $viajes[$seguir]->mostrarDatos();
+                    echo $funciono;
+                    break;
+
+                case 2: //Cambiar el codigo del viaje
+                    echo "Escriba nuevo codigo de viaje: ";
+                    $nuevoCodigo = cargarNumerico ();
+                    $viajes[$seguir]->cambiarCodigo($nuevoCodigo);
+                    echo "El cambio fue realizado con exito. \n";
+                    echo "\n";
+                    break;
+                case 3: //Cambiar el destino del viaje
+                        echo "Escriba el nuevo destino: ";
+                        $nuevoDestino = cargarLetras();
+                        echo "\n";
+                        $viajes[$seguir]->cambiarDestino($nuevoDestino);
+                        echo "El cambio fue realizado con exito. \n";
+                        echo "\n";
+                    break;
+                case 4: //Cambiar el maximo de pasajeros
+                        echo "Escriba el nuevo numero maximo de pasajeros: ";
+                        $nuevoMaximo = cargarNumerico();
+                        $viajes[$seguir]->cambiarCantPasajeros($nuevoMaximo);
+                        echo "El cambio fue realizado con exito. \n";
+                        echo "\n";
+                    break;
+                case 5: //Cambiar el nombre de un pasajero
+                        echo "Escriba el DNI del pasajero: ";
+                        $dni = cargarNumerico();
+                        echo "\n";
+                        echo "Escriba el nuevo nombre del pasajero: ";
+                        $nombre = cargarLetras();
+                        echo "\n";
+                        $funciono = $viajes[$seguir]->cambiarNombrePas($dni,$nombre);
+                        echo $funciono;
+                    break;
+                case 6: //Cambiar el apellido de un pasajero
+                        echo "Escriba el DNI del pasajero: ";
+                        $dni = cargarNumerico();
+                        $valido = true;
+                        echo "\n";
+                        echo "Escriba el nuevo apellido del pasajero: ";
+                        $apellido = cargarLetras();
+                        echo "\n";
+                        $funciono = $viajes[$seguir]->cambiarApellidoPas($dni,$apellido); 
+                        echo $funciono; 
+                    break;
+                case 7: //Cambiar el DNI de un pasajero
+                        echo "Escriba el DNI del pasajero: ";
+                        $dni = cargarNumerico();
+                        echo "\n";
+                        echo "Escriba el nuevo DNI del pasajero: ";
+                        $newDni = cargarNumerico();
+                        echo "\n";
+                        $funciono = $viajes[$seguir]->cambiarDniPas($dni,$newDni);
+                        echo $funciono;
+                    break;
+                case 8: //Cambiar todos los datos de un pasajero ||| PREGUNTAR SI ES MEJOR ANIDAR TODOS LOS IF?????
+                        echo "Escriba el DNI del pasajero a ser reemplazado: ";
+                        $dni = cargarNumerico();
+                        echo "\n";
+                        echo "Escriba el nuevo nombre del pasajero: ";
+                        $nombre = cargarLetras();
+                        echo "\n";
+                        echo "Escriba el nuevo apellido del pasajero: ";
+                        $apellido = cargarLetras();
+                        echo "\n";
+                        echo "Escriba el nuevo DNI del pasajero: ";
+                        $newDni = cargarNumerico();
+                        echo "\n";
+                        $funciono = $viajes[$seguir]->cambiarPasajero($dni,$nombre,$apellido,$newDni);
+                        echo $funciono;
+                    break;
+                case 9: //Agregar un nuevo pasajero
+                        echo "Ingrese el nombre del pasajero a agregar: \n";
+                        $nombre = cargarLetras();
+                        echo "\n";
+                        echo "Ingrese el apellido del pasajero a agregar: \n";
+                        $apellido = cargarLetras();
+                        echo "\n";
+                        echo "Ingrese el dni del pasajero a agregar: \n";
+                        $dni = cargarNumerico();
+                        echo "\n";
+                        $funciono = $viajes[$seguir]->agregarPasajero($nombre,$apellido,$dni);
+                        echo $funciono;
+                    break;
+                case 11: //salir del menu
+                    echo "\n";
+                    echo "Viaje Feliz le agradace por ultilizar sus servicios. \n";
+                    echo "\n";
+                    break;
+            }
+        } while (!($opcion == 11 ) && !($opcion == 10)); //en el caso del 10, solo cortaria este do, y regresaria al primer menu
+    }
+} while (!($opcion == 11 ));
---
+++ Viajes.php
@@ -0,0 +1,178 @@
+<?php
+//Creo la clase viaje
+class Viaje {
+    //Declaro sus atributos
+    private $codigoViaje;
+    private $destino;
+    private $cantidadPasajeros;
+    private $datosPasajeros;
+
+    //creo el meotodo constructor de la clase
+    public function __construct ($codigo, $destinoV, $cantPasajeros, $datosPas){
+        //if (count($datosPas) <= $cantPasajeros){//PREGUNTTARRRRRR------------------------- CANCELADO PQ LO VALIDO EN OTRO LADO
+        $this->codigoViaje = $codigo;
+        $this->destino = $destinoV;
+        $this->cantidadPasajeros = $cantPasajeros;
+        $this->datosPasajeros = $datosPas;
+        //}
+        //else {
+           // echo "No puede crearse el viaje debido a la cantidad de pasajeros exede el maximo posible. \n";
+            //echo "\n";
+       // }
+    }
+
+    //Creo metodos de acceso a los atributos 
+    public function getCodigoViaje (){
+        return $this->codigoViaje;
+    }
+
+    public function getDestino (){
+        return $this->destino;
+    }
+
+    public function getCantidadPasajeros (){
+        return $this->cantidadPasajeros;
+    }
+
+    public function getDatosPasajeros (){
+        return $this->datosPasajeros;
+    }
+
+    //Creo metodos de seteo
+    public function setCodigoViaje ($nuevoCodigo){
+        $this->codigoViaje = $nuevoCodigo;
+    }
+
+    public function setDestino ($nuevoDestino){
+        $this->destino = $nuevoDestino;
+    }
+
+    public function setDatosPasajeros ($nuevosDatos){
+        $this->datosPasajeros = $nuevosDatos;
+    }
+
+    public function setCantidadPasajeros ($nuevaCantPasajeros){
+        $this->cantidadPasajeros = $nuevaCantPasajeros;
+    }
+
+    //Creo un metodo que permite modificar el Codigo del viaje
+    public function cambiarCodigo ($newCodigo){
+        $this->setCodigoViaje($newCodigo);
+    } 
+
+    //Creo un metodo que permite modificar el destino del viaje
+    public function cambiarDestino ($newDestino){
+        $this->setDestino($newDestino);
+    } 
+
+    //Creo un metodo que permite modificar la cantidad de pasajerosecho "\n";
+    public function cambiarCantPasajeros ($newCantPas){
+        $this->setCantidadPasajeros($newCantPas);
+    } 
+
+    //Creo un metodo para modificar dni de los pajeros
+    public function cambiarNombrePas  ($dniPasajero, $nuevoNombre){
+        $rta = ""; //es valido usar una misma variable en todos los metodos? nunca se usan dos metodos al mismo tiempo no deberia tener conflicto, y no creo variables en exeso al usar solo una, dudasssssss
+        $fueEncontrado = false;
+        $arrayfunct = $this->getDatosPasajeros();
+        for ($i=0; $i < count($this->getDatosPasajeros ()) ; $i++) { 
+            if (strcmp($dniPasajero,$this->getDatosPasajeros()[$i]["DNI"]) === 0){
+                $arrayfunct [$i]["Nombre"] = $nuevoNombre;
+                $i = count($this->getDatosPasajeros ());
+                $rta = "El cambio se realizo con exito. \n"."\n";
+                $fueEncontrado = true;
+                $this->setDatosPasajeros ($arrayfunct);
+            }
+        }
+        if (!$fueEncontrado) {
+            $rta = "No se encontro el pasajero con el DNI: ".$dniPasajero." \n"."\n";
+        }
+        return $rta;
+    }
+
+     //Creo un metodo para modificar el apellido de los pajeros
+     public function cambiarApellidoPas  ($dniPasajero, $nuevoApellido){
+        $rta = "";
+        $arrayfunct = $this->getDatosPasajeros();
+        for ($i=0; $i < count($this->getDatosPasajeros ()) ; $i++) { 
+            if (strcmp($dniPasajero,$this->getDatosPasajeros ()[$i]["DNI"]) === 0){
+                $arrayfunct [$i]["Apellido"] = $nuevoApellido;
+                $i = 20000000;
+                $rta = "El cambio se realizo con exito. \n"."\n";
+                $this->setDatosPasajeros ($arrayfunct);
+            }
+        }
+        if ($i == count($this->getDatosPasajeros ())){
+            $rta = "El cambio no pudo realizarse, es posible que el DNI del pasajero a modificar no sea valido, vuelva a intentarlo. \n"."\n";
+        }
+        return $rta;
+    }
+
+     //Creo un metodo para modificar el DNI de los pajeros
+     public function cambiarDniPas  ($dniPasajero, $nuevoDni){
+        $rta = "";
+        $arrayfunct = $this->getDatosPasajeros();
+        for ($i=0; $i < count($this->getDatosPasajeros ()) ; $i++) { 
+            if (strcmp($dniPasajero,$this->getDatosPasajeros ()[$i]["DNI"]) === 0){
+                $arrayfunct[$i]["DNI"] = $nuevoDni;
+                $i = 20000000;
+                $rta = "El cambio se realizo con exito. \n"."\n";
+                $this->setDatosPasajeros ($arrayfunct);
+            }
+        }
+        if ($i == count($this->getDatosPasajeros ())){
+            $rta = "El cambio no pudo realizarse, es posible que el DNI del pasajero a modificar no sea valido, vuelva a intentarlo. \n"."\n";
+        }
+        return $rta;
+    }
+
+    public function cambiarPasajero ($dniPasajero, $nuevoNombre, $nuevoApellido, $nuevoDni){
+        $rta = "";
+        $arrayfunct = $this->getDatosPasajeros();
+        for ($i=0; $i < count($this->getDatosPasajeros ()) ; $i++) { 
+            if (strcmp($dniPasajero,$this->getDatosPasajeros ()[$i]["DNI"]) === 0){
+                $arrayfunct[$i]["Nombre"] = $nuevoNombre;
+                $arrayfunct[$i]["Apellido"] = $nuevoApellido;
+                $arrayfunct[$i]["DNI"] = $nuevoDni;
+                $i = 20000000;
+                $rta = "El cambio se realizo con exito. \n"."\n";
+                $this->setDatosPasajeros ($arrayfunct);
+            }
+        }
+        if ($i == count($this->getDatosPasajeros ())){
+            $rta = "El cambio no pudo realizarse, es posible que el DNI del pasajero a modificar no sea valido, vuelva a intentarlo. \n"."\n";
+        }
+        return $rta;
+    }
+
+    public function agregarPasajero ($nuevoNombre, $nuevoApellido, $nuevoDni){
+        $rta = "";
+        if (($this->getCantidadPasajeros () - count($this->getDatosPasajeros ())) > 0 ) { 
+            $datos = $this->getDatosPasajeros ();
+            $new = count ($this->getDatosPasajeros ());
+            array_push ( $datos , ["Nombre"=>$nuevoNombre, "Apellido"=> $nuevoApellido, "DNI"=>$nuevoDni]);
+            $this->setDatosPasajeros($datos);
+            //array_push ($this->getDatosPasajeros () ); //no se puede usar?????
+            $rta = "El pasajero se agrego con exito. \n"."\n";
+            
+        }
+        else {
+            $rta = "No se puede agregar el pasajero debido a que el viaje llego a su capacidad maxima. \n"."\n";
+        }
+        return $rta;
+    }
+
+    //creo una funcion para poder mostrar todos los datos del viaje.
+    public function mostrarDatos(){ // esto seria el __toStrign???, que tan mal esta que le llame de otra manera?
+        $informacion = "";
+        $informacion = $informacion."El codigo del viaje es: ".$this->getCodigoViaje()."\n";
+        $informacion = $informacion."El destino del viaje es: ".$this->getDestino()."\n";
+        $informacion = $informacion."La cantidad maxima de pasajeros es de: ".$this->getCantidadPasajeros()."\n";
+        $informacion = $informacion."La lista de los ".count($this->getDatosPasajeros ())." pasajeros es: \n"."\n";
+        for ($i = 0; $i < count($this->getDatosPasajeros ()); $i++) { 
+            $informacion = $informacion."Nombre: ".$this->getDatosPasajeros ()[$i]["Nombre"]."\n"."Apellido: ".$this->getDatosPasajeros ()[$i]["Apellido"]."\n"."DNI: ".$this->getDatosPasajeros ()[$i]["DNI"]."\n"."\n";
+        }
+        return $informacion;
+    }
+
+}
